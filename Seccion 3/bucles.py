d2bd9defda81032b8d0cafab6977517a2a60e2b0
carlosdev11 <carlosprimorico02@gmail.com>
2024-11-05 13:46:12 +0100

    ordeno por secciones

---
+++ Seccion 3/bucles.py
@@ -0,0 +1,25 @@
+#CICLO WHILE
+
+contador = 1
+while contador <= 5:
+    print(contador)
+    contador += 1 #incrementa la variable en 1
+    
+    
+#CICLO FOR
+cadena = "hola mundo"
+for letra in cadena:
+    print(letra, end=" ")
+     
+    
+frutas = ["platano","fresa","melon"]
+for fruta in frutas:
+    print(fruta)
+    
+    
+numero = 1
+suma = 0
+while numero < 6:
+    suma += numero
+    numero += 1
+print(suma)
